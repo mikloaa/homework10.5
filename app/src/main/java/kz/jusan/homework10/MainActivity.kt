2c8e407ce250447e417088008eae1c4ad03cb8ec
mikloaa <dda.mma2000@mail.ru>
2022-12-30 18:53:36 +0600

    Add Pin colors

--- app/src/main/java/kz/jusan/homework10/MainActivity.kt
+++ app/src/main/java/kz/jusan/homework10/MainActivity.kt
@@ -1,26 +1,37 @@
 package kz.jusan.homework10
 
 import android.app.ActivityManager.TaskDescription
+import android.graphics.Color
+import android.graphics.Color.BLACK
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import kotlin.math.PI
 
 const val  CORRECT_PIN = "1567"
 const val PIN_LENGTH = 4
 class MainActivity : AppCompatActivity() {
     private var pinText  = ""
+    lateinit var tvPin: TextView
 
+    var errorColor: Int = Color.BLACK
+    var pinTextColor: Int = Color.BLACK
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initTvPin()
         initNumButtons()
         initDeleteButton()
         initOkButton()
+        initColors()
+    }
+    private fun initTvPin(){
+        tvPin = findViewById(R.id.tv_pin)
     }
 
     private fun initDeleteButton(){
@@ -31,6 +42,12 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun initColors(){
+        errorColor= ContextCompat.getColor(this, R.color.error_color)
+        pinTextColor =  ContextCompat.getColor(this, R.color.color_primary)
+    }
+
     private fun initOkButton(){
         val btnOk: Button = findViewById(R.id.btn_OK)
         btnOk.setOnClickListener{checkIfPinIsCorrect() }
@@ -38,6 +55,9 @@ class MainActivity : AppCompatActivity() {
     private fun checkIfPinIsCorrect(){
         if (pinText == CORRECT_PIN){
             Toast.makeText(this, R.string.correct, Toast.LENGTH_LONG).show()
+        } else {
+
+            tvPin.setTextColor(errorColor)
         }
     }
     private fun initNumButtons(){
@@ -83,11 +103,10 @@ class MainActivity : AppCompatActivity() {
         updatePinTextView()
     }
     private fun updatePinTextView(){
-
-        val tvPin:TextView = findViewById(R.id.tv_pin)
         if (pinText.length> PIN_LENGTH){
             pinText = pinText.substring(0, PIN_LENGTH)
         }
         tvPin.text = pinText
+        tvPin.setTextColor(pinTextColor)
     }
 }
