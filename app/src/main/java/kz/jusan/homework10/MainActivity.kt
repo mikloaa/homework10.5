bfd1c25813867657f1d2e03e261cab7ee1b000b3
mikloaa <dda.mma2000@mail.ru>
2022-12-30 18:26:20 +0600

    Added PinCode Logic

--- app/src/main/java/kz/jusan/homework10/MainActivity.kt
+++ app/src/main/java/kz/jusan/homework10/MainActivity.kt
@@ -1,11 +1,88 @@
 package kz.jusan.homework10
 
+import android.app.ActivityManager.TaskDescription
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
+const val  CORRECT_PIN = "1567"
 class MainActivity : AppCompatActivity() {
+    private var pinText  = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initNumButtons()
+        initDeleteButton()
+        initOkButton()
+    }
+
+    private fun initDeleteButton(){
+        val btnDelete:Button = findViewById(R.id.btn_delete)
+        btnDelete.setOnClickListener{
+            pinText = pinText.dropLast(1)
+            updatePinTextView()
+        }
+
+    }
+    private fun initOkButton(){
+        val btnOk: Button = findViewById(R.id.btn_OK)
+        btnOk.setOnClickListener{checkIfPinIsCorrect() }
+    }
+    fun checkIfPinIsCorrect(){
+        if (pinText == CORRECT_PIN){
+            Toast.makeText(this, R.string.correct, Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun initNumButtons(){
+        val btnOne: Button = findViewById(R.id.btn_one)
+        btnOne.setOnClickListener(this::onNumButtonClick)
+
+        val btnTwo: Button = findViewById(R.id.btn_two)
+        btnTwo.setOnClickListener(this::onNumButtonClick)
+
+        val btnThree: Button = findViewById(R.id.btn_three)
+        btnThree.setOnClickListener(this::onNumButtonClick)
+
+        val btnFour: Button = findViewById(R.id.btn_four)
+        btnFour.setOnClickListener(this::onNumButtonClick)
+
+        val btnFive: Button = findViewById(R.id.btn_five)
+        btnFive.setOnClickListener(this::onNumButtonClick)
+
+        val btnSix: Button = findViewById(R.id.btn_six)
+        btnSix.setOnClickListener(this::onNumButtonClick)
+
+        val btnSeven: Button = findViewById(R.id.btn_seven)
+        btnSeven.setOnClickListener(this::onNumButtonClick)
+
+        val btnEight: Button = findViewById(R.id.btn_eight)
+        btnEight.setOnClickListener(this::onNumButtonClick)
+
+        val btnNine: Button = findViewById(R.id.btn_nine)
+        btnNine.setOnClickListener(this::onNumButtonClick)
+
+        val btnZero: Button = findViewById(R.id.btn_zero)
+        btnZero.setOnClickListener(this::onNumButtonClick)
+    }
+
+    private fun onNumButtonClick(view: View) {
+        if (view !is Button){
+            return
+        }
+
+        val  clickedNum = view.text
+        pinText += clickedNum
+
+        updatePinTextView()
+    }
+    private fun updatePinTextView(){
+
+        val tvPin:TextView = findViewById(R.id.tv_pin)
+        tvPin.text = pinText
     }
 }
